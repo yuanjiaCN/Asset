64f724d6dea5296815e20e28731f03eb592df6a0
liuhaoyu1 <33304323+liuhaoyu1@users.noreply.github.com>
2018-01-25 17:02:11 +0800

    commit need

--- app/containers/Profile/Need/index.js
+++ app/containers/Profile/Need/index.js
@@ -1,70 +1,54 @@
 import React,{PureComponent} from 'react'
+import { Table, Badge, Menu, Dropdown, Icon,Upload, message, Button, Tabs, Input,Cascader  } from 'antd';
+import moment from 'moment';
+import './styles.less';
+import BTList from '../../../components/BTList'
+const TabPane = Tabs.TabPane;
 
-import BTAssetCell from './subviews/AssetCell'
-import {Pagination} from 'antd'
+import BTAssetDetail from './subviews/BTAssetDetail'
+import BTUploadAsset from './subviews/BTUploadAsset'
+import BTHaveBought from "./subviews/BTHaveBought"
+import BTMyAssetSet from "./subviews/BTMyAssetSet"
 
-import BTMyTag from '../../../components/BTMyTag'
-
-const BTHeaderSearch = () => (
-    <div className="searchViewStyle">
-        <div>
-            <BTMyTag>全部</BTMyTag>
-            <BTMyTag>图像</BTMyTag>
-            <BTMyTag>数据清洗</BTMyTag>
-
-            <BTMyTag>全部</BTMyTag>
-            <BTMyTag>视频</BTMyTag>
-            <BTMyTag>音频</BTMyTag>
-            <BTMyTag>图片</BTMyTag>
-        </div>
-        <div style={{marginTop:20}}>
-        <BTMyTag>全部</BTMyTag>
-        <BTMyTag>数据挖掘</BTMyTag>
-        <BTMyTag>图像</BTMyTag>
-        <BTMyTag>数据清洗</BTMyTag>
-
-        <BTMyTag>全部</BTMyTag>
-        <BTMyTag>视频</BTMyTag>
-        <BTMyTag>音频</BTMyTag>
-        </div>
-    </div>
-) 
-
-export default class BTProfileNeed extends PureComponent{
+export default class BTProfileAsset extends PureComponent{
     constructor(props){
         super(props)
     }
 
     render(){
+
         return(
-            <div>
-                <BTHeaderSearch/>
+            <div className="assetTagPage">
+                <Tabs>
+                    <TabPane tab="发布需求" key="1">
+                        <BTUploadAsset/>
+                    </TabPane>
+                    <TabPane tab="已发布需求" key="2">
+                        <BTAssetDetail/>
+                    </TabPane>
+                    <TabPane tab="已购买需求" key="3">
+                        <BTHaveBought/>
+                    </TabPane>
 
-                <div style={{marginTop:20}}>
-                    <ul>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-                        <li><BTAssetCell linkto="/assets/detail"/></li>
-
-                    </ul>
-                </div>
-
-                <div style={{marginBottom:20}}>
-                    <Pagination defaultCurrent={1} total={50} />
-                </div>
+                    <TabPane tab="我的需求库" key="4">
+                        <BTMyAssetSet/>
+                    </TabPane>
+                </Tabs>
             </div>
         )
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
